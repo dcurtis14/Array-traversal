dd874b0a6ea90a0dae6452e68ab09e5d9589937d
dcurtis14 <donald.curtis@patelhs.org>
2020-11-02 14:34:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    string[]names = {"Seraph", "Naruto", "Ichigo", "Senpai", "Kanye", "Mikasa", "lil pump", "Boku","Corona"};
+
+    for (int i = 0; i<6; i++){
+      System.out.println(names[i]);
+    }
+
+   names [6] = "Big pump";
+
+System.out.println("here is our updated array:");
+
+for(int i = 0; i<7; i++){
+  System.out.println(names[i]);
+}
+
+
+
+
+  }
+}
